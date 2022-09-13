c236ef18a60bdb21e72b582cf11ec730e27d033d
laurent <laurent.lesage51@gmail.com>
2022-09-13 14:33:14 +0200

    in progress

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -15,25 +15,28 @@ class DefaultControllerTest extends WebTestCase
         // Request a specific page
         $crawler = $client->request('GET', '/');
 
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+
         // Validate a successful response and some content
         $this->assertResponseIsSuccessful();
-        
-        $this->assertSelectorTextContains('a','Créer un utilisateur');
-        $this->assertSelectorTextContains('a','Créer une nouvelle tâche');
-        $this->assertSelectorTextContains('a','Consulter la liste des tâches à faire');
-        $this->assertSelectorTextContains('a','Consulter la liste des tâches terminées');
 
-        $client->clickLink('Créer un utilisateur');
-        $this->assertStringContainsString('/users/create', $client->getRequest()->getRequestUri());
+        // $this->assertSelectorTextContains('a','Créer un utilisateur');
+        // $this->assertSelectorTextContains('a','Créer une nouvelle tâche');
+        // $this->assertSelectorTextContains('a','Consulter la liste des tâches à faire');
+        // $this->assertSelectorTextContains('a','Consulter la liste des tâches terminées');
 
-        $client->clickLink('Créer une nouvelle tâche');
-        $this->assertStringContainsString('/tasks/create', $client->getRequest()->getRequestUri());
+        // $client->clickLink('Créer un utilisateur');
+        // $this->assertStringContainsString('/users/create', $client->getRequest()->getRequestUri());
+
+        // $client->clickLink('Créer une nouvelle tâche');
+        // $this->assertStringContainsString('/tasks/create', $client->getRequest()->getRequestUri());
 
         $client->clickLink('Consulter la liste des tâches à faire');
         $this->assertStringContainsString('/tasks/to_do', $client->getRequest()->getRequestUri());
 
-        $client->clickLink('Consulter la liste des tâches terminées');
-        $this->assertStringContainsString('/tasks/completed', $client->getRequest()->getRequestUri());
+        // $client->clickLink('Consulter la liste des tâches terminées');
+        // $this->assertStringContainsString('/tasks/completed', $client->getRequest()->getRequestUri());
 
     }
 
--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
@@ -18,21 +19,50 @@ class UserControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $client->request('GET', '/users/create');
+
         $this->assertSelectorTextContains('button', 'Ajouter');
         $this->assertStringContainsString('/users/create', $client->getRequest()->getRequestUri());
 
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+
+        // Validate a successful response and some content
+        // $this->assertResponseIsSuccessful();
 
         $crawler = $client->submitForm(
-            'Nom d\'utilisateur', ['user[username]' => 'Laurent'],
-            'Adresse email', ['user[email]' => 'laurent.lesage51@gmail.com'],        
-            'Mot de passe', ['user[password][first]' => 'hello'], 
-            'Tapez le mot de passe à nouveau', ['user[password][second]' => 'hello'], 
-            'Role selection', ['user[roleSelection]' => ['ROLE_ADMIN']],              
-                // 'user[_token]' => ''                       
+            'Ajouter',
+            [
+                'user[username]' => 'Laurent',
+                'user[password][first]' => 'hello',
+                'user[password][second]' => 'hello',
+                'user[roleSelection]' => 'ROLE_ADMIN'
+            ],
+                    
         );
-
+        dd($client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $client->followRedirect();  
         $this->assertStringContainsString('/', $client->getRequest()->getRequestUri());  
     }
+
+
+    //admin access
+
+    // public function testGetListUsersPage(): void
+    // {
+    //     $client = static::createClient();
+    //     $userRepository = static::getContainer()->get(UserRepository::class);
+
+    //     // retrieve the test user
+    //     $testUser = $userRepository->findOneByEmail('john.doe@example.com');
+
+    //     // simulate $testUser being logged in
+    //     $client->loginUser($testUser);
+
+    //     // test e.g. the profile page
+    //     $client->request('GET', '/admin/users');
+    //     $this->assertResponseIsSuccessful();
+    //     $this->assertSelectorTextContains('h2', 'Liste des utilisateurs');
+    // }
 
 }
